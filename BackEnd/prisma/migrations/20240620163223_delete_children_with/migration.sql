b97ce2e6610174f69819bf8094165d4c75125536
Oluwatosin Oyekeye <oluwatosin@meta.com>
2024-07-03 13:58:02 -0700

    Deleted Backend

--- BackEnd/prisma/migrations/20240620163223_delete_children_with/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Cards" DROP CONSTRAINT "Cards_boardId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Cards" ADD CONSTRAINT "Cards_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
